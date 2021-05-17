82af6be3f34f551466f7f79a014f796a17243ac9
miramos-sage <ext-Miguel.RAMOS@sage.com>
2021-05-17 19:54:10 +0200

    Frontend and Backend

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -1,18 +1,36 @@
-import express from 'express'
-import data from './data'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
+
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/products/:id", (req, res)=>{
-    const productId = req.params.id;
-    const product = data.products.find(x=>x._id === productId);
-    if(product){
-        res.send(product);
-    }
-    else res.status(404).send({msg: "Product not found"})
-})
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 
-app.get("/api/products", (req, res)=>{
-    res.send(data.products);
-})
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
-app.listen(5000,()=>{ console.log("Server stated at http://localhost:5000")})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
